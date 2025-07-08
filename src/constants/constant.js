6fab3cae6ea87bbecc8274095acda2228f890ebc
sharon-lee <rosharonl22@gmail.com>
2025-07-08 23:15:39 +0900

    constant추가

---
+++ src/constants/constant.js
@@ -0,0 +1,19 @@
+const CONST ={
+    "IMG_BASE_URL" : 'https://imagedelivery.net/gS0qNsW-qTEWGPRRSa1ghg',
+    "VARIANT" : "public", 
+    "ELEMENT_EARTH" : '2e8c70bf-70e9-4e1f-d704-e7715de49500',
+    "ELEMENT_DARK" : '8d36c974-7f4c-4b36-d0fe-74e361f51900',
+    "ELEMENT_WATER" : '209038b1-c337-4b47-d0f6-34f0d39bff00',
+    "ELEMENT_LIGHT" : 'd06a0c90-2d9c-494f-935b-824e88c24600',
+    "ELEMENT_FIRE" : '5a9cffaa-18c3-41de-e592-f0c5fde37000',
+    "JOB_GUARDIAN" : 'f71b8e59-5d45-4dfa-0f05-4c2db6c34f00',
+    "JOB_SHOOTER" : '18d3edbb-dbc4-4c21-170e-d51a14c11300',
+    "JOB_STRIKER" : '8bf2034e-bc14-4d19-340f-ab8ca1cd6c00',
+    "JOB_COMMANDER" : '322de6e1-70e6-4dd3-9411-3e6739e5fb00',
+    "JOB_PRIST" : '078efb2d-379d-43da-3e8b-a5d67e463300',
+    "JOB_WARRIOR" : '2f48b4a0-de14-435c-733b-c4ea93a47700',
+    "PLACEHOLDER" :  'ff3d1a89-c477-44e5-5f2b-1db1ea80e800'
+
+}
+
+export default CONST;
