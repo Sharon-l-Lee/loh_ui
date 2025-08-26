904d22a9cb5522df9f0c39ae8804b193dd95b9cd
sharon-lee <rosharonl22@gmail.com>
2025-08-26 16:35:11 +0900

    지연로딩 재적용

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -305,6 +305,7 @@
                       :src="getImgUrl(skill.cf_url)" 
                       class="w-16 h-16 rounded-lg object-cover"
                       alt="스킬 이미지"
+                      loading="lazy"
                     />
 
                   <!-- 오른쪽 텍스트 영역 -->
@@ -356,6 +357,7 @@
                     :src="getImgUrl(af.cf_url)" 
                     class="w-16 h-16 rounded-lg object-cover shrink-0"
                     alt="아티팩트 이미지"
+                    loading="lazy"
                   />
 
                   <!-- 오른쪽 텍스트 영역 -->
