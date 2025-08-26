9830aaca1121905b164616d0b4a9b0d82cf87995
sharon-lee <rosharonl22@gmail.com>
2025-08-26 15:34:24 +0900

    성능측정을 위해 되돌림

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -305,7 +305,6 @@
                       :src="getImgUrl(skill.cf_url)" 
                       class="w-16 h-16 rounded-lg object-cover"
                       alt="스킬 이미지"
-                      loading="lazy"
                     />
 
                   <!-- 오른쪽 텍스트 영역 -->
@@ -355,7 +354,6 @@
                   <!-- 아티팩트 이미지 -->
                   <img
                     :src="getImgUrl(af.cf_url)" 
-                    loading="lazy"
                     class="w-16 h-16 rounded-lg object-cover shrink-0"
                     alt="아티팩트 이미지"
                   />
